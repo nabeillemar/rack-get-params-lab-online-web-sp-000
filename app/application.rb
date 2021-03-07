be452fc7708307cea520219b53107c4c1fbe5e38
Nabeil <nabeillemar@gmail.com>
2021-03-06 21:14:36 -0800

    commit tree d19f9a37cf33a6d56dc77c8c6281a37a833ffaba

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,23 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart == []
+        resp.write "Your cart is empty!"
+      else 
+        @@cart.each do |c|
+          resp.write "#{c}\n"
+        end 
+      end 
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      if @@items.include?(add_item)
+        @@cart << add_item
+        resp.write "added #{add_item}"
+      else 
+        resp.write "We don't have that item"
+      end 
+
     else
       resp.write "Path Not Found"
     end
@@ -27,4 +44,6 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  @@cart = []
 end
